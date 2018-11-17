1c0dd83be68e59499c9bec82340d1556d97ac6b6
Samuel Gélineau <gelisam+github@gmail.com>
2018-11-17 12:04:13 -0500

    usage

--- compile.sh
+++ compile.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+if [ ! -f "$1" ]; then
+  echo "usage: $0 src/Category.agda"
+  echo
+  echo "A poor man's IDE. Recompiles the given file when it changes."
+  exit 1
+fi
+
 clear
 agda \
   --library standard-library \
--- compile_continuously.sh
+++ compile_continuously.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-./compile.sh "$@" || true
-fswatcher --throttle 300 --path "$@" -- ./compile.sh "$@"
+if [ ! -f "$1" ]; then
+  echo "usage: $0 src/Category.agda"
+  echo
+  echo "A poor man's IDE. Recompiles the given file when it changes."
+  exit 1
+fi
+
+./compile.sh "$1" || true
+fswatcher --throttle 300 --path "$1" -- ./compile.sh "$1"
